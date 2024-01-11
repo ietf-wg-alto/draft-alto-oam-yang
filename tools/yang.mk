9180ace1a49235508109cc688838cae9b548f12f
jensenzhang <jingxuan.n.zhang@gmail.com>
2024-01-11 10:09:38 +0800

    Repeat 'if-feature' to eliminate yanglint errors
    Some redundant 'if-feature's may not be necessary for libyang2, but are
    needed to pass the validation of libyang1.
    
    Signed-off-by: jensenzhang <jingxuan.n.zhang@gmail.com>

--- tools/yang.mk
+++ tools/yang.mk
@@ -4,6 +4,8 @@ STDYANGDIR ?= tools/yang
 $(STDYANGDIR):
 	git clone --depth 10 -b main https://github.com/YangModels/yang $@
 
+LIBYANGBIN ?= yanglint
+
 OPTIONS=--tree-print-groupings --tree-no-expand-uses -f tree --tree-line-length=67
 ifeq ($(OS), Windows_NT)
 YANG_PATH="$(YANGDIR);$(STDYANGDIR)/standard/ietf/RFC/;$(STDYANGDIR)/experimental/ietf-extracted-YANG-modules"
@@ -15,6 +17,7 @@ STDYANG=$(wildcard $(YANGDIR)/ietf-*.yang)
 EXPYANG=$(wildcard $(YANGDIR)/example-*.yang)
 EXPJSON=$(wildcard $(YANGDIR)/example-*.json)
 TXT=$(patsubst $(YANGDIR)/%.yang,%-diagram.txt,$(YANG))
+YANGLINT=$(patsubst $(YANGDIR)/ietf-%.yang,yang-lint-%.log,$(STDYANG))
 
 .PHONY: yang-lint yang-gen-diagram yang-clean
 
@@ -22,13 +25,15 @@ pyang-lint: $(STDYANG) $(EXPYANG) $(STDYANGDIR)
 	pyang -V --ietf -p $(YANG_PATH) $(STDYANG)
 	pyang -V -p $(YANG_PATH) $(EXPYANG)
 
-yang-lint: $(STDYANG) $(STDYANGDIR)
-	yanglint --verbose -p $(YANGDIR) -p $(STDYANGDIR)/standard/ietf/RFC/ -p $(STDYANGDIR)/experimental/ietf-extracted-YANG-modules $(STDYANG) -i
+yang-lint-%.log: $(YANGDIR)/ietf-%.yang
+	$(LIBYANGBIN) --verbose -p $(YANGDIR)/temp -p $(YANGDIR) -p $(STDYANGDIR)/standard/ietf/RFC/ -p $(STDYANGDIR)/experimental/ietf-extracted-YANG-modules $< -i -o $@
+
+yang-lint: $(YANGLINT)
 
 yang-gen-diagram: yang-lint $(TXT)
 
 yang-clean:
-	rm -f $(TXT)
+	rm -f $(TXT) $(YANGLINT)
 
 yangson-validate: $(EXPJSON) $(STDYANGDIR)
 	yangson -p $(YANGDIR) -p $(YANGDIR):$(STDYANGDIR)/standard/ietf/RFC/:$(STDYANGDIR)/experimental/ietf-extracted-YANG-modules -v $(EXPJSON) $(YANGDIR)/yang-library-ietf-alto.json
